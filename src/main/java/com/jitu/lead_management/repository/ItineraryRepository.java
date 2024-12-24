49d0ce8cb5d5481f5f502acffe45c2c4712625a8
Aditya kumawat <92208854+Adikumaw@users.noreply.github.com>
2024-12-24 19:30:20 +0530

    added readme.md and exposed fetch itinerary names api and handle duplicate template names exception (#31)
    * Create README.md (#28)
    
    * exposed fetch itinerary names api and handle duplicate template names exception

--- src/main/java/com/jitu/lead_management/repository/ItineraryRepository.java
+++ src/main/java/com/jitu/lead_management/repository/ItineraryRepository.java
@@ -3,6 +3,7 @@ package com.jitu.lead_management.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jitu.lead_management.entity.Itinerary;
@@ -11,5 +12,8 @@ import com.jitu.lead_management.entity.Itinerary;
 public interface ItineraryRepository extends JpaRepository<Itinerary, Integer> {
 
     void deleteByItineraryIdIn(List<Integer> intItineraryIds);
+
+    @Query("SELECT i.templateName FROM Itinerary i")
+    List<String> fetchTemplateNames();
 
 }
