2cbb8a0b8125c3b27a5486f639b00dcfe45b8488
all_father <kumawataditya105@gmail.com>
2024-12-26 13:10:48 +0530

    added @Modifying and @Transactional to ItineraryRepository.java

--- src/main/java/com/jitu/lead_management/repository/ItineraryRepository.java
+++ src/main/java/com/jitu/lead_management/repository/ItineraryRepository.java
@@ -3,14 +3,19 @@ package com.jitu.lead_management.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jitu.lead_management.entity.Itinerary;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface ItineraryRepository extends JpaRepository<Itinerary, Integer> {
 
+    @Modifying
+    @Transactional
     void deleteByItineraryIdIn(List<Integer> intItineraryIds);
 
     @Query("SELECT i.templateName FROM Itinerary i")
